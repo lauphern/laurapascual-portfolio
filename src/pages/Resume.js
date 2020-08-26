d5ee14eb44d439f322c67785d651ff86b07c6bd7
lauphern <lau.p.hern@hotmail.com>
2020-08-26 14:09:01 +0200

    added swagger ui component to Resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,10 +1,10 @@
 import React from 'react';
+import SwaggerUI from "swagger-ui-react"
+import "swagger-ui-react/swagger-ui.css"
 
 const Resume = props => {
-
   return(
-    <>
-    </>
+    <SwaggerUI url="https://resume-api.vercel.app/definition.yaml"/>
   )
 }
 
