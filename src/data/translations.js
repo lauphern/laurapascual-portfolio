2880498c9eb39775eb06afcedb9f55b2f396e7f8
lauphern <lau.p.hern@hotmail.com>
2020-08-26 17:01:29 +0200

    added resume to translations

--- src/data/translations.js
+++ src/data/translations.js
@@ -1,10 +1,12 @@
 export const translations = {
   en: {
     subtitle: "Full-stack developer",
+    cv: "Résumé",
     linkedin: "https://www.linkedin.com/in/laura-pascual-hernandez/?locale=en_US",
   },
   es: {
     subtitle: "Desarrolladora Full-stack",
+    cv: "Currículum",
     linkedin: "https://www.linkedin.com/in/laura-pascual-hernandez/?locale=es_ES",
   },
 };
