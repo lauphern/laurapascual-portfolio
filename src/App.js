037e465c4b7964cb3eda1cf6dcdb36f7ebac1b5a
lauphern <lau.p.hern@hotmail.com>
2020-08-26 14:29:03 +0200

    implemented Home page

--- src/App.js
+++ src/App.js
@@ -1,49 +1,21 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { ThemeProvider } from "@material-ui/core/styles";
-import useMediaQuery from "@material-ui/core/useMediaQuery";
-import { Container, Grid } from "@material-ui/core";
 import "./App.scss";
 
 import { Switch, Route } from "react-router-dom";
+import Home from "./pages/Home";
 import Resume from "./pages/Resume";
-import Bio from "./components/Bio";
-import Links from "./components/Links";
 
 import { theme } from "./style/theme";
-import { useAppStyles } from "./style/useStyles";
-import { _cursorEffect } from "./utils/cursorEffect";
 
 function App() {
-  const classes = useAppStyles();
-
-  const isItSmallDevice = useMediaQuery("(max-width:320px)");
-  const isItSmallTablet = useMediaQuery("(max-width:834px)");
-  const isItTablet = useMediaQuery("(max-width:1100px)");
-  const isItSmallerThanLaptop = useMediaQuery("(max-width:1199px)");
-
-  useEffect(() => {
-    if (!isItSmallerThanLaptop)
-      document.querySelector(".hover-container").addEventListener("mousemove", _cursorEffect);
-    else document.querySelector(".hover-container").removeEventListener("mousemove", _cursorEffect);
-  });
 
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
         <Switch>
           <Route exact path="/">
-            <div className="hover-container"></div>
-            <Container className={`${classes.mainContainer} ${classes.layout}`} disableGutters>
-              <Grid
-                className={classes.layout}
-                container
-                alignContent="center"
-                justify="space-between"
-              >
-                <Bio isItSmallDevice={isItSmallDevice} isItSmallTablet={isItSmallTablet} />
-                <Links isItSmallTablet={isItSmallTablet} isItTablet={isItTablet} />
-              </Grid>
-            </Container>
+            <Home />
           </Route>
           <Route path="/resume">
             <Resume />
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,2 +1,38 @@
-//TODO
-//move the homepage here
+import React, { useEffect } from "react";
+import { Container, Grid } from "@material-ui/core";
+import useMediaQuery from "@material-ui/core/useMediaQuery";
+
+import Bio from "../components/Bio";
+import Links from "../components/Links";
+
+import { useAppStyles } from "../style/useStyles";
+import { _cursorEffect } from "../utils/cursorEffect";
+
+const Home = props => {
+  const classes = useAppStyles();
+
+  const isItSmallDevice = useMediaQuery("(max-width:320px)");
+  const isItSmallTablet = useMediaQuery("(max-width:834px)");
+  const isItTablet = useMediaQuery("(max-width:1100px)");
+  const isItSmallerThanLaptop = useMediaQuery("(max-width:1199px)");
+
+  useEffect(() => {
+    if (!isItSmallerThanLaptop)
+      document.querySelector(".hover-container").addEventListener("mousemove", _cursorEffect);
+    else document.querySelector(".hover-container").removeEventListener("mousemove", _cursorEffect);
+  });
+
+  return (
+    <>
+      <div className="hover-container"></div>
+      <Container className={`${classes.mainContainer} ${classes.layout}`} disableGutters>
+        <Grid className={classes.layout} container alignContent="center" justify="space-between">
+          <Bio isItSmallDevice={isItSmallDevice} isItSmallTablet={isItSmallTablet} />
+          <Links isItSmallTablet={isItSmallTablet} isItTablet={isItTablet} />
+        </Grid>
+      </Container>
+    </>
+  );
+};
+
+export default Home;
