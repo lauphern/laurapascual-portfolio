c7d918c0ba8000939edf0f676f71997eb4dfd03d
lauphern <lau.p.hern@hotmail.com>
2020-08-27 01:57:12 +0200

    added animations to the Resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,7 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link as RouterLink } from "react-router-dom";
+import { useTransition, animated } from "react-spring";
 import { Container, Button } from "@material-ui/core";
-import KeyboardReturnSharpIcon from '@material-ui/icons/KeyboardReturnSharp';
+import KeyboardReturnSharpIcon from "@material-ui/icons/KeyboardReturnSharp";
 import SwaggerUI from "swagger-ui-react";
 import "swagger-ui-react/swagger-ui.css";
 
@@ -22,27 +23,54 @@ const Resume = props => {
   const resumeClasses = useResumeStyles();
   const appClasses = useAppStyles();
 
+  // const [items, set] = useState([...])
+  const [show, set] = useState(true);
+  const headerTransition = useTransition(show, null, {
+    from: { transform: "translate3d(0,-200px,0)", opacity: 0 },
+    enter: { transform: "translate3d(0,0px,0)", opacity: 1 },
+    leave: { transform: "translate3d(0,-200px,0)", opacity: 0 },
+  });
+  const resumeTransition = useTransition(show, null, {
+    from: { transform: "translate3d(0,200px,0)", opacity: 0 },
+    enter: { transform: "translate3d(0,0px,0)", opacity: 1 },
+    leave: { transform: "translate3d(0,200px,0)", opacity: 0 },
+  });
+
   return (
     <>
       {/* TODO the lang prop is only temporary */}
       <Container className={resumeClasses.headerContainer} disableGutters>
-        <Container className={resumeClasses.headerText} disableGutters>
-          <Header lang="es" />
-          <Button
-            variant="outlined"
-            className={`${appClasses.routerBtn}`}
-            component={RouterLink}
-            to="/"
-            size="small"
-          >
-      {/* TODO the lang prop is only temporary */}
-            <KeyboardReturnSharpIcon /> {translations["es"].home}
-          </Button>
-        </Container>
+        {headerTransition.map(
+          ({ item, props, key }) =>
+            item && (
+              <animated.div key={key} style={props}>
+                <Container className={resumeClasses.headerText} disableGutters>
+                  <Header lang="es" />
+                  <Button
+                    variant="outlined"
+                    className={`${appClasses.routerBtn}`}
+                    component={RouterLink}
+                    to="/"
+                    size="small"
+                  >
+                    {/* TODO the lang prop is only temporary */}
+                    <KeyboardReturnSharpIcon /> {translations["es"].home}
+                  </Button>
+                </Container>
+              </animated.div>
+            )
+        )}
       </Container>
-      <Container className={resumeClasses.resumeContainer}>
-        <SwaggerUI url="https://resume-api.vercel.app/definition.yaml" />
-      </Container>
+      {resumeTransition.map(
+        ({ item, props, key }) =>
+          item && (
+            <animated.div key={key} style={props}>
+              <Container className={resumeClasses.resumeContainer}>
+                <SwaggerUI url="https://resume-api.vercel.app/definition.yaml" />
+              </Container>
+            </animated.div>
+          )
+      )}
     </>
   );
 };
