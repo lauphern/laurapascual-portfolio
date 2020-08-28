be89fdb5e1af5b01bd03417e647c87c5b9c65d4d
lauphern <lau.p.hern@hotmail.com>
2020-08-29 00:40:30 +0200

    setup i18n

--- src/i18n.js
+++ src/i18n.js
@@ -1,12 +1,10 @@
 import i18n from 'i18next';
-import Backend from 'i18next-http-backend';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+import { translations } from "./data/translations";
+
 i18n
-  // load translation using http -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-http-backend
-  .use(Backend)
   // detect user language
   // learn more: https://github.com/i18next/i18next-browser-languageDetector
   .use(LanguageDetector)
@@ -15,6 +13,7 @@ i18n
   // init i18next
   // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
+    resources: translations,
     fallbackLng: 'en',
     debug: true,
 
