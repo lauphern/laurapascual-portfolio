fcd44548356b20bc9560e04320c8a84e14f3df3f
lauphern <lau.p.hern@hotmail.com>
2020-10-21 01:07:53 +0200

    progress counter logic v1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -11,6 +11,12 @@ const Home = props => {
 
   useEffect(() => {
     // props.setDomReady(true);
+    let i = 0;
+    const counter = setInterval(() => {
+      document.querySelector("#domLoader > span").innerText = i;
+      i += 2;
+      if (i > 100) return clearInterval(counter);
+    }, 100);
     setTimeout(() => {
       props.setDomReady(true);
       document.querySelector("#domLoader").style.display = "none";
